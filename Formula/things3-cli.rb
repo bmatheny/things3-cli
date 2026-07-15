967a5ae66cc230ad9e971f004958803596403182
Blake Matheny <bmatheny@mobocracy.net>
2026-07-15 07:24:03 -0500

    Updated formula

--- Formula/things3-cli.rb
+++ Formula/things3-cli.rb
@@ -1,15 +1,15 @@
 class Things3Cli < Formula
   desc "CLI for Things 3"
-  homepage "https://github.com/ossianhempel/things3-cli"
-  version "0.3.0"
+  homepage "https://github.com/bmatheny/things3-cli"
+  version "0.4.0-beta"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ossianhempel/things3-cli/releases/download/v0.3.0/things-0.3.0-darwin-arm64.tar.gz"
-      sha256 "a6464403832457a935d7b7131d358802d5b751f4c7d25a06725b7bd3dad4e68f"
+      url "https://github.com/bmatheny/things3-cli/releases/download/v0.4.0-beta/things-0.4.0-beta-darwin-arm64.tar.gz"
+      sha256 "912931b0264224e70d668d91e77d35eb24da30a92bcbd6a3baba8c17aa2d6220"
     else
-      url "https://github.com/ossianhempel/things3-cli/releases/download/v0.3.0/things-0.3.0-darwin-amd64.tar.gz"
-      sha256 "235dc9f0a60499d392457114a52b83d5643045ee1020d9f3fa0a97444c6a4197"
+      url "https://github.com/bmatheny/things3-cli/releases/download/v0.4.0-beta/things-0.4.0-beta-darwin-amd64.tar.gz"
+      sha256 "c710e965afe662f4c793555b29e98c3d86c8c17d530405ac863801e28c140dc2"
     end
   end
 
